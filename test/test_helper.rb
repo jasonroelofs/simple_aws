e3c9c9315c0ebce3fc981073cc2893742609fe4e
Jason Roelofs <jameskilton@gmail.com>
2011-12-10 15:05:31 -0500

    Use mocha for mocking

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,13 @@ require 'rubygems'
 gem 'minitest'
 require 'minitest/autorun'
 
+require 'mocha_standalone'
+
 class MiniTest::Unit::TestCase
+  include Mocha::API
+
+  def teardown
+    mocha_verify
+    mocha_teardown
+  end
 end
