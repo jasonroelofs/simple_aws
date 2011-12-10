86d246644f98ef3d2612ae3dd4dc077762b8d7a8
Jason Roelofs <jameskilton@gmail.com>
2011-12-10 16:18:22 -0500

    Build AWS signature and add it to the request
    Doesn't quite work yet.

--- lib/aws/connection.rb
+++ lib/aws/connection.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'openssl'
 
 module AWS
 
@@ -26,10 +27,9 @@ module AWS
     # Will raise if the request has an error
     ##
     def call(request)
-
-      HTTP.get(
+      HTTP.post(
         @api.uri,
-        :query => process_request(request)
+        :body => process_request(request)
       )
     end
 
@@ -44,6 +44,40 @@ module AWS
         "Timestamp" => Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "Version" => @api.version
       }.merge request.params
+
+      params["Signature"] = escape(Base64.encode64(sign_request(params.clone)).chomp)
+
+      p params
+
+      params
+    end
+
+    def sign_request(params)
+      list = params.to_a
+      list.sort! do |a, b|
+        if a[0] == "AWSAccessKeyId"
+          -1
+        else
+          a[0] <=> b[0]
+        end
+      end
+
+      to_sign = "POST\n#{@api.uri}\n\n#{list.map {|p| p.join("=") }.join("\n&")}"
+      puts to_sign
+
+      digest = OpenSSL::Digest::Digest.new("sha256")
+      puts digest
+
+      sig = OpenSSL::HMAC.digest(digest, @api.secret_key, to_sign)
+      puts sig
+      sig
+    end
+
+    # AWS URI escaping, as implemented by Fog
+    def escape(string)
+      string.gsub(/([^a-zA-Z0-9_.\-~]+)/) {
+        "%" + $1.unpack("H2" * $1.bytesize).join("%").upcase
+      }
     end
 
   end
--- test/aws/connection_test.rb
+++ test/aws/connection_test.rb
@@ -14,18 +14,22 @@ describe AWS::Connection do
     it "takes a Request and runs the appropriate request" do
       api = stub_everything
       api.stubs(:access_key).returns("access key")
+      api.stubs(:secret_key).returns("secret key")
       api.stubs(:uri).returns("URL")
       api.stubs(:version).returns("2011-01-01")
 
-      AWS::HTTP.expects(:get).with do |path, options|
+      AWS::HTTP.expects(:post).with do |path, options|
         path.must_equal "URL"
 
-        options[:query]["Action"].must_equal "request_action"
-        options[:query]["Version"].must_equal "2011-01-01"
-        options[:query]["AWSAccessKeyId"].must_equal "access key"
-        options[:query]["SignatureMethod"].must_equal "HmacSHA256"
-        options[:query]["SignatureVersion"].must_equal "2"
-        Time.parse(options[:query]["Timestamp"]).wont_be_nil
+        options[:body]["Action"].must_equal "request_action"
+        options[:body]["Version"].must_equal "2011-01-01"
+        options[:body]["AWSAccessKeyId"].must_equal "access key"
+        options[:body]["SignatureMethod"].must_equal "HmacSHA256"
+        options[:body]["SignatureVersion"].must_equal "2"
+
+        options[:body]["Signature"].wont_be_nil
+
+        Time.parse(options[:body]["Timestamp"]).wont_be_nil
         true
       end
 
