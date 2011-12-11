e7d87812e432f4fa5c25edb04198b0cadea26433
Jason Roelofs <jameskilton@gmail.com>
2011-12-11 16:57:25 -0500

    Allow querying Request through ruby-esque methods

--- lib/aws/response.rb
+++ lib/aws/response.rb
@@ -16,9 +16,89 @@ module AWS
   end
 
   ##
-  # Wrapper object for all responses from AWS.
+  # Wrapper object for all responses from AWS. This class gives
+  # a lot of leeway to how you access the response object.
+  # You can access the response directly through it's Hash representation,
+  # which is a direct mapping from the raw XML returned from AWS.
+  #
+  # You can also use ruby methods. This object will convert those methods
+  # in ruby_standard into appropriate keys (camelCase) and look for them
+  # in the hash. This can be done at any depth.
+  #
+  # One special case handled is the "Set" data types. If this class finds
+  # a nested object with the "item" key, it will take care of the mapping so
+  # that instead of writing
+  #
+  #   response.reservation_set.item.first.reservation_id (DescribeInstances)
+  #
+  # you'll be able to write the above like:
+  #
+  #   response.reservation.first.reservation_id
+  #
+  # Outside of this, this class tries not to be too magical to ensure that
+  # it never gets in the way. All nested objects are queryable like their
+  # parents are, and all sets and arrays are found and accessible through
+  # your typical Enumerable interface.
+  #
+  # This class will also ensure that any Set is always an Array, given that
+  # when AWS returns a single item in a set, the xml -> hash parser gives a
+  # single hash back instead of an array.
   ##
   class Response
+
+    # Inner proxy class that handles converting ruby methods
+    # into keys found in the underlying Hash.
+    class ResponseProxy
+      include Enumerable
+
+      def initialize(local_root)
+        if local_root.keys == ["item"]
+          # Ensure "item" is ignored and it's children are always
+          # turned into an array.
+          @local_root = [local_root["item"]].flatten.map do |entry|
+            ResponseProxy.new(entry)
+          end
+        else
+          @local_root = local_root
+        end
+      end
+
+      def [](key_or_idx)
+        value_or_proxy(@local_root[key_or_idx] || @local_root[key_or_idx + "Set"])
+      end
+
+      def length
+        @local_root.length
+      end
+
+      def each(&block)
+        @local_root.each(&block)
+      end
+
+      def method_missing(name, *args)
+        if value = value_for_key_matching(name)
+          value_or_proxy value
+        else
+          super
+        end
+      end
+
+      protected
+
+      def value_for_key_matching(name)
+        base_aws_name = AWS::Util.camelcase name.to_s, :lower
+        @local_root[base_aws_name] ||
+          @local_root[base_aws_name + "Set"]
+      end
+
+      def value_or_proxy(value)
+        if value.is_a?(Hash) || value.is_a?(Array)
+          ResponseProxy.new value
+        else
+          value
+        end
+      end
+    end
 
     ##
     # The raw parsed response body in Hash format
@@ -37,7 +117,7 @@ module AWS
 
       @body = http_response.parsed_response
 
-      @request_body = @body[@body.keys.first]
+      @request_root = ResponseProxy.new @body[@body.keys.first]
     end
 
     ##
@@ -47,20 +127,14 @@ module AWS
     # attributes that matter
     ##
     def [](key)
-      @request_body[key]
+      @request_root[key]
     end
 
     ##
-    # Allow mapping of ruby-esque method calls to the parameters
-    # in the response.
+    # Delegate first-level method calls to the root Proxy object
     ##
     def method_missing(name, *args)
-      aws_name = AWS::Util.camelcase name.to_s, :lower
-      if value = @request_body[aws_name]
-        value
-      else
-        super
-      end
+      @request_root.send(name, *args)
     end
 
   end
--- test/aws/response_test.rb
+++ test/aws/response_test.rb
@@ -44,7 +44,6 @@ describe AWS::Response do
 
       @http_response = stub
       @http_response.stubs(:success?).returns(true)
-      @http_response.stubs(:code).returns(200)
       @http_response.stubs(:parsed_response).returns(@response_hash)
 
       @response = AWS::Response.new @http_response
@@ -69,6 +68,98 @@ describe AWS::Response do
       @response["volumeId"].must_equal "v-12345"
       @response["domain"].must_equal "vpc"
       @response["unknownKey"].must_be_nil
+    end
+
+  end
+
+  describe "nested responses and arrays" do
+    before do
+      @response_hash = {
+        "CommandResponse" => {
+          "xmlns" => "some url",
+          "requestId" => "1234-Request-Id",
+          "simpleNestedObject" => {
+            "name" => "Here's something deeper"
+          },
+          "singleItemResultsSet" => {
+            "item" => {"keyId" => "1234", "domain" => "vpc"},
+          },
+          "multipleItemsSet" => {
+            "item" => [
+              {"keyId" => "1234"},
+              {"keyId" => "5678"},
+              {"keyId" => "9012"}
+            ]
+          },
+          "multipleDepthSet" => {
+            "key" => "value",
+            "simpleInnerSet" => {
+              "item" => {"range" => "14"}
+            },
+            "complexInnerSet" => {
+              "item" => [
+                {"range" => "12"},
+                {"range" => "274", "deeperSet" => { "item" => {"hiddenItem" => "42"}}}
+              ]
+            }
+          }
+        }
+      }
+
+      @http_response = stub
+      @http_response.stubs(:success?).returns(true)
+      @http_response.stubs(:parsed_response).returns(@response_hash)
+
+      @response = AWS::Response.new @http_response
+    end
+
+    describe "method calls" do
+
+      it "allows querying multiple objects deep" do
+        @response.simple_nested_object.name.must_equal "Here's something deeper"
+      end
+
+      it "allows querying of a result set with one item" do
+        @response.single_item_results.length.must_equal 1
+        @response.single_item_results.first.key_id.must_equal "1234"
+        @response.single_item_results.first.domain.must_equal "vpc"
+      end
+
+      it "allows enumerating through a result set with lots of items" do
+        @response.multiple_items.length.must_equal 3
+        @response.multiple_items[0].key_id.must_equal "1234"
+        @response.multiple_items[1].key_id.must_equal "5678"
+        @response.multiple_items[2].key_id.must_equal "9012"
+      end
+
+      it "allows diving into a nested result set" do
+        @response.multiple_depth.simple_inner_set.first.range.must_equal "14"
+        @response.multiple_depth.complex_inner_set[1].deeper[0].hidden_item.must_equal "42"
+      end
+    end
+
+    describe "hash keys" do
+      it "allows querying multiple objects deep" do
+        @response["simpleNestedObject"]["name"].must_equal "Here's something deeper"
+      end
+
+      it "allows querying of a result set with one item" do
+        @response["singleItemResults"].length.must_equal 1
+        @response["singleItemResults"][0]["keyId"].must_equal "1234"
+        @response["singleItemResults"][0]["domain"].must_equal "vpc"
+      end
+
+      it "allows enumerating through a result set with lots of items" do
+        @response["multipleItems"].length.must_equal 3
+        @response["multipleItems"][0]["keyId"].must_equal "1234"
+        @response["multipleItems"][1]["keyId"].must_equal "5678"
+        @response["multipleItems"][2]["keyId"].must_equal "9012"
+      end
+
+      it "allows diving into a nested result set" do
+        @response["multipleDepth"]["simpleInnerSet"][0]["range"].must_equal "14"
+        @response["multipleDepth"]["complexInnerSet"][1]["deeper"][0]["hiddenItem"].must_equal "42"
+      end
     end
 
   end
