079e6356aee46fc3636b267e563ce636e44d1288
Jason Roelofs <jameskilton@gmail.com>
2011-12-19 17:16:24 -0500

    Add very basic ELB sample

---
+++ samples/elb.rb
@@ -0,0 +1,16 @@
+$: << File.expand_path("../../lib", __FILE__)
+
+require 'aws/elb'
+
+##
+# Expects your Amazon keys to be in the environment, something like
+#
+# export AWS_KEY="KEY"
+# export AWS_SECRET="SECRET"
+##
+
+$elb = AWS::ELB.new ENV["AWS_KEY"], ENV["AWS_SECRET"]
+
+puts "", "Your Load Balancers", ""
+
+p $elb.describe_load_balancers
