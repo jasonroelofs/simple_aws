b90d27f71786b733bddc1422688d3e8ebd65c24c
Jason Roelofs <jameskilton@gmail.com>
2011-12-20 17:28:16 -0500

    Update ELB sample to show usage

--- samples/elb.rb
+++ samples/elb.rb
@@ -13,4 +13,14 @@ $elb = AWS::ELB.new ENV["AWS_KEY"], ENV["AWS_SECRET"]
 
 puts "", "Your Load Balancers", ""
 
-p $elb.describe_load_balancesr
+$elb.describe_load_balancers.describe_load_balancers_result.load_balancer_descriptions.each do |elb|
+  puts "Name: #{elb.load_balancer_name}"
+  puts "HealthCheck: #{elb.health_check.inspect}"
+
+  elb.listener_descriptions.each do |desc|
+    l = desc.listener
+    puts "Listener: #{l.protocol}:#{l.load_balancer_port} => #{l.instance_protocol}:#{l.instance_port}"
+  end
+
+  puts ""
+end
