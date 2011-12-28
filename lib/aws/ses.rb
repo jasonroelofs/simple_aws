ef146cedd32b7f8fd7ccd805ba95134ee372666c
Jason Roelofs <jameskilton@gmail.com>
2011-12-28 15:01:27 -0500

    Implement Simple Email Service API

---
+++ lib/aws/ses.rb
@@ -0,0 +1,29 @@
+require 'aws/api'
+require 'aws/call_types/action_param'
+require 'aws/signing/version3'
+
+module AWS
+
+  ##
+  # Amazon's Simple Email Service
+  #
+  # http://docs.amazonwebservices.com/ses/latest/APIReference/
+  #
+  # All requests are POST and always through HTTPS.
+  ##
+  class SES < API
+    endpoint "email"
+    use_https true
+    version "2010-12-01"
+    default_region "us-east-1"
+
+    # SES only has one HTTP endpoint
+    def initialize(key, secret)
+      super(key, secret)
+    end
+
+    include CallTypes::ActionParam
+    include Signing::Version3
+  end
+
+end
