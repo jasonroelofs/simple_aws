f3fbab107d5380a3c2a1d09b48a29d24f2cd105f
Jason Roelofs <jameskilton@gmail.com>
2012-01-09 14:22:45 -0500

    Add CloudFront sample

---
+++ samples/cloud_front.rb
@@ -0,0 +1,17 @@
+$: << File.expand_path("../../lib", __FILE__)
+
+require 'aws/cloud_front'
+
+##
+# Expects your Amazon keys to be in the environment, something like
+#
+# export AWS_KEY="KEY"
+# export AWS_SECRET="SECRET"
+##
+
+cloud_front = AWS::CloudFront.new ENV["AWS_KEY"], ENV["AWS_SECRET"]
+
+puts "First ten distribution items:"
+
+p cloud_front.get("/distribution", "MaxItems" => 10)
+
