9b645b138091ffe385aba11ee02902c93d100240
Jason Roelofs <jameskilton@gmail.com>
2012-03-22 17:55:57 -0400

    Update XML util test to work on all platforms
    JRuby's version of Nokogiri doesn't print any whitespace in the XML while
    MRI does, so we just ensure all whitespace is cleared out when checking the
    results.

--- test/simple_aws/core/util_test.rb
+++ test/simple_aws/core/util_test.rb
@@ -3,16 +3,15 @@ require 'simple_aws/core/util'
 
 describe SimpleAWS::Util do
 
+  def remove_whitespace_from(string)
+    string.gsub(/^\s*/, "").gsub(/\n/, "")
+  end
+
   describe "#build_xml_from" do
     it "takes a hash and builds XML" do
       response = SimpleAWS::Util.build_xml_from :RootNode => { :InnerNode => "Value" }
 
-      response.must_equal <<END
-<?xml version="1.0" encoding="UTF-8"?>
-<RootNode>
-  <InnerNode>Value</InnerNode>
-</RootNode>
-END
+      remove_whitespace_from(response).must_equal %|<?xml version="1.0" encoding="UTF-8"?><RootNode><InnerNode>Value</InnerNode></RootNode>|
     end
 
     it "will add namespace to the root node" do
@@ -21,12 +20,7 @@ END
         "http://cloudfront.amazonaws.com/doc/2010-11-01/"
       )
 
-      response.must_equal <<END
-<?xml version="1.0" encoding="UTF-8"?>
-<RootNode xmlns="http://cloudfront.amazonaws.com/doc/2010-11-01/">
-  <InnerNode>Value</InnerNode>
-</RootNode>
-END
+      remove_whitespace_from(response).must_equal %|<?xml version="1.0" encoding="UTF-8"?><RootNode xmlns="http://cloudfront.amazonaws.com/doc/2010-11-01/"><InnerNode>Value</InnerNode></RootNode>|
     end
 
     it "works with arrays of items" do
@@ -34,14 +28,7 @@ END
         {:RootNode => { :InnerNode => ["Value1", "Value2", "Value3"] }}
       )
 
-      response.must_equal <<END
-<?xml version="1.0" encoding="UTF-8"?>
-<RootNode>
-  <InnerNode>Value1</InnerNode>
-  <InnerNode>Value2</InnerNode>
-  <InnerNode>Value3</InnerNode>
-</RootNode>
-END
+      remove_whitespace_from(response).must_equal %|<?xml version="1.0" encoding="UTF-8"?><RootNode><InnerNode>Value1</InnerNode><InnerNode>Value2</InnerNode><InnerNode>Value3</InnerNode></RootNode>|
     end
 
     it "works at any nestedness of hashes" do
@@ -54,17 +41,7 @@ END
         }
       )
 
-      response.must_equal <<END
-<?xml version="1.0" encoding="UTF-8"?>
-<RootNode>
-  <InnerNode>
-    <Child>Value1</Child>
-  </InnerNode>
-  <InnerNode>
-    <Child>Value2</Child>
-  </InnerNode>
-</RootNode>
-END
+      remove_whitespace_from(response).must_equal %|<?xml version="1.0" encoding="UTF-8"?><RootNode><InnerNode><Child>Value1</Child></InnerNode><InnerNode><Child>Value2</Child></InnerNode></RootNode>|
     end
 
     it "auto-strings all leaf nodes" do
